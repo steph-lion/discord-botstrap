15c47a79935a88f8c489b986f729982c94f42010
Stefano Leone <stefano.leone99@gmail.com>
2025-04-13 02:17:12 +0200

    chore: Standardize quotes in configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 // eslint-disable-next-line no-undef
 module.exports = {
-  testEnvironment: "node",
-  preset: "ts-jest",
-  testMatch: ["**/*.test.ts"],
-  testPathIgnorePatterns: ["/node_modules/"],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
